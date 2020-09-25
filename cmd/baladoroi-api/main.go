a177d0911f09069217ce0f3f340786a0db24c80a
Dimitris Glaros <glaros.dimitrios@gmail.com>
2020-09-25 22:50:00 +0300

    Change main.go to implement gorm db migration

--- cmd/baladoroi-api/main.go
+++ cmd/baladoroi-api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dimglaros/baladoroi-api/pkg/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("hello")
+	dsn := os.Getenv("DATABASE_USER") + ":" + os.Getenv("DATABASE_PASSWORD") + "@tcp(" + os.Getenv("DATABASE_HOST") + ":" + os.Getenv("DATABASE_PORT") + ")/" + os.Getenv("DATABASE_NAME") + "?parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.AutoMigrate(&models.User{}, &models.Credentials{}, &models.Field{}, &models.Game{})
+	if err != nil {
+		panic(err.Error())
+	}
 }
