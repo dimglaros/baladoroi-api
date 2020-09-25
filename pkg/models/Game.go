32c6c5544429a4b1f380f2e59097f63c263c61f2
Dimitris Glaros <glaros.dimitrios@gmail.com>
2020-09-25 22:58:07 +0300

    Change UserID to HostID in Game model

--- pkg/models/Game.go
+++ pkg/models/Game.go
@@ -9,7 +9,8 @@ type Game struct {
 	Base
 	ScheduledAt  time.Time
 	TeamSize     uint
-	UserID       uuid.UUID `gorm:"type:char(36);column:host_id"`
+	HostID       uuid.UUID `gorm:"type:char(36)"`
+	Host         User
 	FieldID      uuid.UUID `gorm:"type:char(36)"`
 	Participants []User    `gorm:"many2many:game_participants"`
 }
