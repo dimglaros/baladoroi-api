00c7f1e211ee446f933c1167a2d59d3c0568de48
Dimitris Glaros <glaros.dimitrios@gmail.com>
2020-10-04 01:41:22 +0300

    Change type of DeleteAt field in Base model

--- pkg/models/Base.go
+++ pkg/models/Base.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID  `gorm:"type:char(36);primary_key;" json:"id"`
-	CreatedAt time.Time  `json:"createdAt"`
-	UpdatedAt time.Time  `json:"updatedAt"`
-	DeletedAt *time.Time `sql:"index" json:"deletedAt"`
+	ID        uuid.UUID      `gorm:"type:char(36);primary_key;" json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `sql:"index" json:"deletedAt"`
 }
 
 func (b *Base) BeforeCreate(*gorm.DB) (err error) {
